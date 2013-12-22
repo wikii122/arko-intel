529787ad42f1bc64790bf55abb3940b2aad2b27f
Wiktor <wikii122@gmail.com>
2013-12-22 20:46:38 +0100

    Main file input control

--- main.cc
+++ main.cc
@@ -2,6 +2,7 @@
 #include "data.cc"
 #define SIZE 201*201
 #define SINGLE 1
+#define PIXEL_SIZE 3
 
 extern "C" int mapa( int* mapa, unsigned char* image, MapaStruct* par);
 extern "C" int przekroj( int* mapa,  unsigned char* image, PrzekStruct* przek );
@@ -15,13 +16,18 @@ int main( int argc, char** argv )
 	BMPHeader header;
 	FILE* file;
 
-	if( argc != 2 )
+	if( argc != 3 )
 	{
 		printf( "Usage:\n\tbin map intersection\n" );
 		return 0;
 	}
 	
 	file = fopen( "mapa.txt", "r" );
+	if( file==NULL )
+	{
+		printf( "File mapa.txt does not exist." );
+		return 1;
+	}
 	for( int i=0; i<SIZE; i++ ) 
 	{
 		fscanf( file, "%d", &value );
@@ -30,11 +36,21 @@ int main( int argc, char** argv )
 	fclose( file );
 
 	file = fopen( "parametry.txt", "r" );
+	if( file==NULL )
+	{
+		printf( "File parametry.txt does not exist." );
+		return 1;
+	}
 	fscanf( file, "%d %d", &ps.x1, &ps.y1 );
 	fscanf( file, "%d %d", &ps.x2, &ps.y2 );
 	fscanf( file, "%d", &ms.min );
 	fscanf( file, "%d", &ms.max );
 	fclose( file );
+	if( ms.min > ms.max )
+	{
+		printf( "Wrong parameters!" );
+		return 0;
+	}
 	ps.ms = &ms;
 	indx = ps.x2>ps.x1? ps.x2-ps.x1:ps.x1-ps.x2;
 	indy = ps.y2>ps.y1? ps.y2-ps.y1:ps.y1-ps.y2;
@@ -48,13 +64,13 @@ int main( int argc, char** argv )
 	file = fopen( argv[1], "w+" );
 	makeMapHeader( &header ); 
 	fwrite( &header, sizeof(BMPHeader), SINGLE, file );
-	fwrite( &mapBMP, 3, SIZE, file );
+	fwrite( &mapBMP, PIXEL_SIZE, SIZE, file );
 	fclose( file );
 
 	file = fopen( argv[2], "w+" );
 	makeInterHeader( &header, ps, range );
 	fwrite( &header, sizeof(BMPHeader), SINGLE, file );
-	fwrite( &interBMP, 3, range*(ms.max-ms.min), file );
+	fwrite( &interBMP, PIXEL_SIZE, range*(ms.max-ms.min), file );
 	fclose( file );
 
 	return 0;
