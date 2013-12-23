62110bf0477fab491cc63ea6fb4bf5cd99c8f03a
Wiktor <wikii122@gmail.com>
2013-12-23 20:36:00 +0100

    Handling minimal and maximal values

--- mapa.asm
+++ mapa.asm
@@ -12,9 +12,10 @@ mapa:
 	; prologue
 	push	ebp				; Store old EBP.
 	mov	ebp,		esp		; Set new EBP.
-	sub	esp,		20		; Five integer local variables.
+	sub	esp,		12		; Five integer local variables.
 
 	; body
+	; Initization of data
 	mov	ebx,		dword [ebp+8]	; Load address of struct informations.
 	mov	eax,		dword [ebx]	; Load minimal value.
 	mov	dword [ebp-4],	eax		; Store minimal value.
@@ -22,17 +23,41 @@ mapa:
 	mov	dword [ebp-8],	eax		; Store maximal value.
 	sub	eax,		dword [ebp-4]	; Count difference between max and min.
 	mov	dword [ebp-12],	eax		; Store difference.
-	mov	eax,		dword [ebp+16]	; Load source address.
-	mov	dword [ebp-16], eax		; Store source address in local variable.
-	mov	eax,		dword [ebp+12]	; Load target address.
-	mov	dword [ebp-20],	eax		; Store target address in local variable.
+
+	; Prepare for loop
+	mov	ecx,		201*201		; Load loop counter
+	mov	esi,		dword [ebp+16]	; Load source address.
+	mov	edi,		dword [ebp+12]	; Load destination address.
+	cld					; Clear destination flag.
+loop1:
+	lodsd					; Load first number into accumulator.
+	mov	ebx,		dword [ebp-8]	; Load maximal value.
+	cmp	eax,		ebx		; Compare value with maximal
+	jge	put_black			; if higher than maximal, simply put black.
+	sub	eax,		dword [ebp-4]	; Substract minimal value from current.
+	js	put_white			; If result negative, simply put white.
+
+loop1_continue:
+	stosd					; Store result value
+	stosd					; for every color in rgb,
+	stosd					; so three times.
+	loop	loop1				; Decrement loop counter and jump loop1 if nonzero.
 
 	; epilogue
-	mov	eax, 	0			; Return 0.
+	mov	eax, 		0		; Return 0.
 
-	add	esp,	12			; Reset ESP.
+	add	esp,		12		; Restore ESP.
 	pop	ebp				; Restore EBP.
 	ret
+
+; Here some helper methods.
+; For loop1:
+put_black:
+	mov	eax,		0		; Store 0s
+	jmp	loop1_continue			; Continue the loop.
+put_white:
+	mov	eax,		-1		; Store 1s
+	jmp	loop1_continue			; Continue loop.
 
 ;============================================
 ; STOS
@@ -53,9 +78,5 @@ mapa:
 ;  | maximal value               | EBP-8
 ;  -------------------------------
 ;  | delta between min and max   | EBP-12
-;  -------------------------------
-;  | pointer to source array	 | EBP-16
-;  -------------------------------
-;  | pointer to target bitmap    | EPB-20, ESP
 ;  -------------------------------
 ;============================================
