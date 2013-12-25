95c8b197d5d32e66a7786cbe001a56155ec9134c
Wiktor <wikii122@gmail.com>
2013-12-25 15:36:36 +0100

    Fixed bad call parameters order

--- mapa.asm
+++ mapa.asm
@@ -16,7 +16,7 @@ mapa:
 
 	; body
 	; Initization of data
-	mov	ebx,		dword [ebp+8]	; Load address of struct informations.
+	mov	ebx,		dword [ebp+16]	; Load address of struct informations.
 	mov	eax,		dword [ebx]	; Load minimal value.
 	mov	dword [ebp-4],	eax		; Store minimal value.
 	mov	eax,		dword [ebx+4]	; Load maximal value.
@@ -26,12 +26,23 @@ mapa:
 
 	; Prepare for loop
 	mov	ecx,		201*201		; Load loop counter
-	mov	esi,		dword [ebp+16]	; Load source address.
+	mov	esi,		dword [ebp+8]	; Load source address.
 	mov	edi,		dword [ebp+12]	; Load destination address.
 	cld					; Clear destination flag.
+	jmp	loop1				; jump to the loop.
+
+put_black:
+	mov	eax,		0		; Store 0s
+	jmp	loop1_continue			; Continue the loop.
+
+put_white:
+	mov	eax,		-1		; Store 1s
+	jmp	loop1_continue			; Continue loop.
+
 loop1:
 	lodsd					; Load first number into accumulator.
-	cmp	eax,		dword [ebp-8[	; Compare value with maximal
+debug:
+	cmp	eax,		dword [ebp-8]	; Compare value with maximal
 	jge	put_black			; if higher than maximal, simply put black.
 	sub	eax,		dword [ebp-4]	; Substract minimal value from current.
 	js	put_white			; If result negative, simply put white.
@@ -43,44 +54,57 @@ loop1:
 	div	dword [ebp-12]			; Normalization of value to 255.
 
 loop1_continue:
-	stosd					; Store result value
-	stosd					; for every color in rgb,
-	stosd					; so three times.
+	mov	byte [edi],	al		; Save result
+	mov	byte [edi+1],	al		; three times,
+	mov	byte [edi+2],	al		; once for each color.
+	add	edi,		3		; and increment target counter.
 	loop	loop1				; Decrement loop counter and jump loop1 if nonzero.
+;TODO: remove
+	jmp	epilogue
+	; Now draw red line.
+	mov	eax,		dword [ebx+20]	; Load first value for y.
+	sub	eax,		dword [ebx+16]	; Count y length of line.
+	mov	dword [ebp-8],	eax		; Store result
 
-	; epilogue
+	mov	ebx,		dword [ebp+8]	; Load data struct address.
+	mov	eax,		dword [ebx+12]	; Load first value for x.
+	sub	eax,		dword [ebp+8]	; Count x length of line.
+	mov	dword [ebp-4],	eax		; Store result
+	idiv	dword [ebp-8]			; Count tangens of the line.
+
+	cmp	eax,		1		; If tangens is greater or equal than 1,
+	jge	draw_by_y			; Line should be drawn by ys.
+	cmp	eax,		-1		; else if tangens
+	jl	draw_by_y			; is less than -1, the also draw by ys.
+draw_by_x:
+draw_by_y:
+	
+; TODO: Swap rows.
+epilogue:
 	mov	eax, 		0		; Return 0.
 
 	add	esp,		12		; Restore ESP.
 	pop	ebp				; Restore EBP.
 	ret
 
-; Here some helper methods.
-; For loop1:
-put_black:
-	mov	eax,		0		; Store 0s
-	jmp	loop1_continue			; Continue the loop.
-put_white:
-	mov	eax,		-1		; Store 1s
-	jmp	loop1_continue			; Continue loop.
 
 ;============================================
 ; STOS
 ;============================================
 ;  |...                          |
-;  | int* mapa                   | EBP+16
+;  | MapaStruct*                 | EBP+16
 ;  ------------------------------
 ;  | char* image                 | EBP+12
 ;  -------------------------------
-;  | MapaStruct* 		 | EBP+8
+;  | int mapa*	 		 | EBP+8
 ;  -------------------------------
 ;  | return address              | EBP+4
 ;  -------------------------------
 ;  | saved ebp                   | EBP
 ;  -------------------------------
-;  | minimal value               | EBP-4
+;  | minimal value, x diff       | EBP-4
 ;  -------------------------------
-;  | maximal value               | EBP-8
+;  | maximal value, y diff       | EBP-8
 ;  -------------------------------
 ;  | delta between min and max   | EBP-12
 ;  -------------------------------
